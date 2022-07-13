ec4147909b873c5cfaee841e26c450269eb3a186
mksmkzk <maksakozak@gmail.com>
2022-07-12 20:52:55 -0700

    Have working Sidebar prototype.

--- static/js/TestingFile.js
+++ static/js/TestingFile.js
@@ -1,52 +1,3 @@
-// Add console.log to check to see if our code is working.
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	maxZoom: 18,
-	accessToken: API_KEY
-});
-
-// We create the second tile layer that will be the background of our map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	maxZoom: 18,
-	accessToken: API_KEY
-});
-
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  maxZoom: 18,
-  accessToken: API_KEY
-});
-
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-	center: [40.7, -94.5],
-	zoom: 3,
-	layers: [dark]
-});
-
-// Create a base layer that holds all three maps.
-let baseMaps = {
-  "Dark": dark,
-  "Streets": streets,
-  "Satellite": satelliteStreets
-};
-
-// Create a layer for our job location data.
-let jobLocations = new L.LayerGroup();
-let orders = new L.LayerGroup();
-let concreteOrders = new L.LayerGroup();
-
-let overlays = {
-  "Job Locations": jobLocations,
-  'Recent Orders': orders,
-  "Concrete Orders": concreteOrders
-};
-
-
-L.control.layers(baseMaps, overlays).addTo(map);
-
 ///////////////////////////////////////////////////////////////////////////////
 // Task: Working with our csv file.                                          //                  
 //                                                                           //
@@ -64,6 +15,9 @@ var JCLocations = {};
 var markers = {};
 var orderMarkers = {};
 var concreteMarkers = {};
+
+// An object of objects to save all the information about the jobs.
+var JCDetails = {};
 
 // Read in the data from our CSV file using d3.
 Promise.all([
@@ -93,11 +47,19 @@ Promise.all([
   var AddMarkers = function(entry)
   {
     JCLocations[entry['Job Code']] = [entry['Latitude'], entry['Longitude']];
+
+    //Update our job details object.
+    JCDetails[entry['Job Code']] = { JobCode : entry['Job Code'],
+                                     JobName : entry['Job Name'],
+                                     JobAddress : entry['Address'],
+                                     JobTags : entry['Tags']};
+
     // Create a new marker for each entry.
     markers[entry['Job Code']] = L.circleMarker([entry.Latitude, entry.Longitude])
       // Adding all our options 
       .addTo(jobLocations)
-      .bindPopup("Job Name: " + entry["Job Name"] + "<br>Job Number: " + entry['Job Code'] + '<br><br>RECENT ORDERS<br>');
+      .on('click', onClick)
+      .bindPopup(entry['Job Code']);
       //+ "<br>Address: " + entry['Address']);
 
   };
@@ -112,27 +74,32 @@ Promise.all([
       if ( entry['Job Number(QProduct)'] !== 'SRVICE' ) {
         
 
-        // TODO: Check if entry exists, if it does update the popup
+        
         if (orderMarkers[entry['Job Number(QProduct)']] === undefined) {
 
           // Get the content of the marker popup.
-          content = markers[entry['Job Number(QProduct)']].getPopup().getContent();
+          // content = markers[entry['Job Number(QProduct)']].getPopup().getContent();
+
+          JCDetails[entry['Job Number(QProduct)']]['JobOrders'] = "Part: " + entry['Part Description'] + "\tQuantity: " + entry['Quantity Shipped'] + "<br>";
 
           orderMarkers[entry['Job Number(QProduct)']] = L.circleMarker(JCLocations[entry['Job Number(QProduct)']])
             .addTo(orders)
-            .bindPopup(content + "Part: " + entry['Part Description'] + "        Quantity: " + entry['Quantity Shipped'] + "<br>")
+            //.bindPopup(content + "Part: " + entry['Part Description'] + "        Quantity: " + entry['Quantity Shipped'] + "<br>")
+            .on('click', onClick)
+            .bindPopup(entry['Job Number(QProduct)'])
             .setStyle({color: 'orange', fillColor: 'orange'});
         }
         else {
-          content = orderMarkers[entry['Job Number(QProduct)']].getPopup().getContent();
-          console.log(content);
+          //content = orderMarkers[entry['Job Number(QProduct)']].getPopup().getContent();
+          // console.log(content);
+
+          JCDetails[entry['Job Number(QProduct)']]['JobOrders'] += "Part: " + entry['Part Description'] + "\tQuantity: " + entry['Quantity Shipped'] + "<br>";
+
           orderMarkers[entry['Job Number(QProduct)']].getPopup().setContent(content + "Part: " + entry['Part Description'] + "        Quantity: " + entry['Quantity Shipped'] + "<br>");
         }
 
         
-        // orderMarkers[entry['Job Number(QProduct)']].getPopup().setContent(
-        //   content + "Part: " + entry['Part Description'] + "        Quantity: " + entry['Quantity Shipped'] + "<br>");
-        // orderMarkers[entry['Job Number(QProduct)']].setStyle({color: 'orange', fillColor: 'orange'}).addTo(orders).removeFrom(jobLocations);
+
         }
       } catch (e) {
         // console.log(e);
@@ -142,7 +109,7 @@ Promise.all([
   // A Function to go through the orders and sort them by Job Number.
   var ExtractOrders2 = function(entry) {
     // DEBUG AREA
-    //console.log(entry['JOB CODE']);
+
 
     // Check if the variable is undefined
     try {
@@ -155,9 +122,6 @@ Promise.all([
           .bindPopup(content + "Total Yards: " + entry['YARDS ORDERED'] + "    Supplier: " + entry['CONCRETE CO'] + "<br>")
           .setStyle({color: 'red', fillColor: 'red'});
         
-        // orderMarkers[entry['JOB CODE']].getPopup().setContent(
-        //   content + "Pump #: " + entry['Pump #'] + "    Yards Ordered: " + entry['YARDS ORDERED'] + "<br>");
-        // orderMarkers[entry['JOB CODE']].setStyle({color: 'red', fillColor: 'red'}).addTo(orders).removeFrom(jobLocations);
         }
       } catch (e) {
         // console.log(e);
@@ -167,7 +131,7 @@ Promise.all([
     // A Function to go through the orders and sort them by Job Number.
     var ExtractOrders3 = function(entry) {
       // DEBUG AREA
-      // console.log(entry['Job code']);
+
   
       // Check if the variable is undefined
       try {
@@ -180,54 +144,40 @@ Promise.all([
           .bindPopup(content + "<br>Total Yards: " + entry['Total Yards'] + "    Supplier: " + entry['Supplier'] + "<br>")
           .setStyle({color: 'red', fillColor: 'red'});
           
-          // concreteMarkers[entry['Job code']].getPopup().setContent(
-          //   content + "<br>Total Yards: " + entry['Total Yards'] + "    Supplier: " + entry['Supplier'] + "<br>");
-          //   concreteMarkers[entry['JOB CODE']].setStyle({color: 'red', fillColor: 'red'}).addTo(concreteOrders).removeFrom(jobLocations);
           }
         } catch (e) {
           // console.log(e);
         }
     };
 
-  console.log(markers)
+  //console.log(markers)
+
+  // A function to be called when the user clicks on a marker.
+  // Will update the sidebar with the information about the marker.
+  function onClick(e) {
+    //console.log(document.getElementById('sidebar-header').innerHTML);
+    // Change the header name of the sidebar to the job code. 
+    var tail = '<span class="sidebar-close"><i class="fa fa-caret-left"></i></span>'
+    var jobCode = e.target.getPopup().getContent();
+
+    const header = document.getElementById('sidebar-header').innerHTML;
+
+    // Update the header of the sidebar.
+    document.getElementById('sidebar-header').innerHTML = jobCode + tail;
+
+    // Update the details section
+    document.getElementById('details').innerHTML = JCDetails[jobCode]['JobName'] + "<br>" + JCDetails[jobCode]['JobAddress'] + "<br>" + JCDetails[jobCode]['JobTags'] + "<br>";
+
+    // Update the recent orders section.
+    document.getElementById('recent-orders').innerHTML = '<b>Recent Orders</b><br>' + JCDetails[jobCode]['JobOrders'];
+    
+    sidebar.open('home');
+  }
   
   files[0].forEach(AddMarkers);
   files[1].forEach(ExtractOrders);
-  files[2].forEach(ExtractOrders2);
-  files[3].forEach(ExtractOrders3);
-
-  // // We are experimenting with the sidebar.
-  // // Finding out what needs to stay and go from the code.
-  // function createMarker(coords, title, info, source) {
-  //   var marker, content;
-  
-  //   content = '<b><font size="6">' + title + '</font></b><br/>' + info + '<br><a href="' + source + '" target="_blank"><button>Orders</button></a>'
-  //   marker = L.marker(coords).addTo(markerLayer);
-  //   marker.bindPopup(content);
-    
-  //   marker.on('click', function(evt) {
-  //     var id = L.Util.stamp(evt.target);
-  //     if (document.getElementById(id) != null) return; 
-  //     var sidebarElement, infoPart, removePart;
-  //     sidebarElement = L.DomUtil.create('div', 'sidebarElement', document.getElementById('sidebar'));
-  //     sidebarElement.id = id;
-  //     infoPart = L.DomUtil.create('div', 'infoSidebarElement', sidebarElement);
-  //     infoPart.innerHTML = content;
-  //     L.DomEvent.on(infoPart, 'click', function(evt) {
-  //       var marker = markerLayer.getLayer(this.id);
-  //       marker.closePopup();
-  //       map.panTo(marker.getLatLng());
-  //       marker.bounce(3);
-  //     }, sidebarElement);
-  //     removePart = L.DomUtil.create('div', 'removeSidebarElement', sidebarElement);
-  //     removePart.innerHTML = 'Remove';
-  //     L.DomEvent.on(removePart, 'click', function(evt) {
-  //       markerLayer.getLayer(this.id).closePopup();
-  //       this.parentNode.removeChild(this);
-  //     }, sidebarElement);
-  //   });
-  // }
-
+  // files[2].forEach(ExtractOrders2);
+  // files[3].forEach(ExtractOrders3);
 
 });
 
